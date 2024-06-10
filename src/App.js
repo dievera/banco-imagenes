7940c4e84486e0561cce9bed875bab65cace9bea
Dievera <diegovera.dg@gmail.com>
2024-06-10 12:59:50 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Card from "./components/Card";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Imagen from "./components/Imagen";
+import Autor from "./components/Autor";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container App">
+      <div class="row">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+              <Route path="Card" element={<Card />} />
+            <Route path="Imagen" element={<Imagen />} />
+            <Route path="Autor" element={<Autor />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
